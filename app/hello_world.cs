c6518b2a96157de074a577816b4034c250b901dc
Sbrendon98 <sylvestre.ba98@gmail.com>
2025-07-28 18:57:28 -0400

    testing

--- app/hello_world.cs
+++ app/hello_world.cs
@@ -11,6 +11,7 @@ namespace Giraffe
             static void Main(string[] args)
             {
                   Console.WriteLine("Hello World");
+                  Console.WriteLine("");
             }
       }
 }
